209753d194a0ea283d9f93ffaa2b0ef1ec0dd9da
unknown <dreilingmarshall@gmail.com>
2019-02-26 18:36:06 -0500

    Additional work on orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,39 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+
+function addQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objtoSearch(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
 var orm = {
     
 // Select all model for page load
     selectAll: function(table, cb) {
-        var queryString = "SELECT * FROM burgers";
+        var queryString = "SELECT * FROM " + table + ";";
         connection.query(queryString), function(err, result) {
             if (err) {
                 throw err;
@@ -16,8 +44,8 @@ var orm = {
     },
     
 // Insert one model for adding burger
-    insertOne: function(burgerName, ingredients) {
-        var queryString = "INSERT INTO burgers";
+    insertOne: function(table, burgerName, ingredients) {
+        var queryString = "INSERT INTO " + table;
     }
 
 
