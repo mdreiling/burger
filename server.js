a79ad7dd05ecb5f4b12b555023c83a04a30407fe
unknown <dreilingmarshall@gmail.com>
2019-02-28 12:54:30 -0500

    Initial commit of handlebars views plus update of server. App currently not working due to callback error

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 // Dependencies
-
 var express = require("express");
 var exsHB = require("express-handlebars");
+
+// Server set up
+var PORT = process.env.PORT || 12000;
+
+var app = express();
+
+// Static content for the app from "public directory"
+app.use(express.static("public"));
+
+// Parse app body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+app.engine("handlebars"), exsHB({ defaultLayout: "main" });
+app.set("view engine", "handlebars");
+
+// Import routes and allow server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// start server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
